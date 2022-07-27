234d8e0282654fc17bfeaf992fd761cdccbec66d
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-07-27 23:47:36 +0100

    feat: added id field to entities

--- src/entities/todo.entity.ts
+++ src/entities/todo.entity.ts
@@ -1,7 +1,8 @@
 import { ObjectId } from "mongoose";
 export interface Todo {
+  _id: string;
   title: string;
-  userId: ObjectId | number;
+  userId: string;
   timestamp: Date;
   status: string;
 }
