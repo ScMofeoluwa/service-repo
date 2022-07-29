dce826b2e9cadea5d303fabbc8c07011e12a9dbd
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-07-29 20:38:22 +0100

    feat: user repository

--- src/repository/user/user.mongo.repository.ts
+++ src/repository/user/user.mongo.repository.ts
@@ -1,4 +1,10 @@
 import { BaseRepository } from "../base/base.mongo.repository";
 import { User } from "../../entities/user.entity";
 
-export class UserRepository extends BaseRepository<User> {}
+export class UserRepository extends BaseRepository<User> {
+  async findByEmail(email: string): Promise<User> {
+    const result = await this._model.findOne({ where: { email: email } });
+    // @ts-expect-error
+    return result;
+  }
+}
