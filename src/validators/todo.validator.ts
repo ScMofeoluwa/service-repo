01e040c4b2672088ef3c1b4c21653972ee61b9a0
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-07-31 23:39:06 +0100

    feat: validators

---
+++ src/validators/todo.validator.ts
@@ -0,0 +1,18 @@
+import Joi from "joi";
+import { Request } from "express";
+
+export function validateCreate(req: Request) {
+  const schema = Joi.object({
+    title: Joi.string().min(5).max(255).required(),
+    status: Joi.string().valid("pending", "completed").required(),
+  });
+  return schema.validate(req);
+}
+
+export function validateUpdate(req: Request) {
+  const schema = Joi.object({
+    title: Joi.string().min(5).max(255),
+    status: Joi.string().valid("pending", "completed"),
+  });
+  return schema.validate(req);
+}
