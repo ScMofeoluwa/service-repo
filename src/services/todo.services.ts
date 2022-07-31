2892938b06ccd5433575731597ae6985c0e801ca
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-07-31 23:38:44 +0100

    feat: services

---
+++ src/services/todo.services.ts
@@ -0,0 +1,32 @@
+import { TodoRepository } from "../repository/todo/todo.mongo.repository";
+import { ITodoService } from "../interfaces/ITodo";
+import { Todo } from "../entities/todo.entity";
+
+export class TodoService implements ITodoService {
+  constructor(private readonly _repository: TodoRepository) {}
+
+  async createTodo(data: Omit<Todo, "_id">): Promise<Todo> {
+    const todo = await this._repository.create(data);
+    return todo;
+  }
+
+  async getTodo(id: string): Promise<Todo> {
+    const todo = await this._repository.findOne(id);
+    return todo;
+  }
+
+  async getTodos(): Promise<Todo[]> {
+    const todos = await this._repository.find();
+    return todos;
+  }
+
+  async deleteTodo(id: string): Promise<boolean> {
+    const result = await this._repository.delete(id);
+    return result;
+  }
+
+  async updateTodo(id: string, data: Partial<Todo>): Promise<boolean> {
+    const result = await this._repository.update(id, data);
+    return result;
+  }
+}
---
+++ src/services/user.services.ts
@@ -0,0 +1,45 @@
+import { UserRepository } from "../repository/user/user.mongo.repository";
+import { IUserService } from "../interfaces/IUser";
+import { genSalt, hash, compare } from "bcryptjs";
+import { User } from "../entities/user.entity";
+
+export class UserService implements IUserService {
+  constructor(private readonly _repository: UserRepository) {}
+
+  async createUser(data: Omit<User, "_id">): Promise<User> {
+    let user = await this._repository.findByEmail(data.email);
+    if (user) {
+      throw new Error("user already exists.");
+    }
+    data.password = await this.hashPassword(data.password);
+    user = await this._repository.create(data);
+    return user;
+  }
+
+  async getUser(id: string): Promise<User> {
+    const user = await this._repository.findOne(id);
+    return user;
+  }
+
+  async getUsers(): Promise<User[]> {
+    const users = await this._repository.find();
+    return users;
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    const result = await this._repository.delete(id);
+    return result;
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    const salt = await genSalt(10);
+    return await hash(salt, password);
+  }
+
+  async isValidPassword(
+    userGivenPassword: string,
+    savedPassword: string
+  ): Promise<boolean> {
+    return await compare(userGivenPassword, savedPassword);
+  }
+}
