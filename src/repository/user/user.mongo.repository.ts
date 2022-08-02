bda2f4b8d33041d4b52850f369071d820ca32bcb
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-08-02 23:50:03 +0100

    DI: user repository

--- src/repository/user/user.mongo.repository.ts
+++ src/repository/user/user.mongo.repository.ts
@@ -1,7 +1,13 @@
 import { BaseRepository } from "../base/base.mongo.repository";
 import { User } from "../../entities/user.entity";
+import { injectable } from "inversify";
+import { userSchema } from "../../schema/user.schema";
 
+@injectable()
 export class UserRepository extends BaseRepository<User> {
+  constructor() {
+    super("users", userSchema);
+  }
   async findByEmail(email: string): Promise<User> {
     const result = await this._model.findOne({ where: { email: email } });
     // @ts-expect-error
