69d9f2962567d3b0322feaaa31e604c7b3e8dc9d
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-08-02 23:49:47 +0100

    DI: todo repository

--- src/repository/todo/todo.mongo.repository.ts
+++ src/repository/todo/todo.mongo.repository.ts
@@ -1,4 +1,11 @@
 import { BaseRepository } from "../base/base.mongo.repository";
 import { Todo } from "../../entities/todo.entity";
+import { todoSchema } from "../../schema/todo.schema";
+import { injectable } from "inversify";
 
-export class TodoRepository extends BaseRepository<Todo> {}
+@injectable()
+export class TodoRepository extends BaseRepository<Todo> {
+  constructor() {
+    super("todos", todoSchema);
+  }
+}
