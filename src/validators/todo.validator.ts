ef69c60ec5ed92c4b0a50299fcff3212a66c381e
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-08-25 19:06:20 +0100

    refactor: todo validator

--- src/validators/todo.validator.ts
+++ src/validators/todo.validator.ts
@@ -4,7 +4,7 @@ import { Request } from "express";
 export function validateCreate(req: Request) {
   const schema = Joi.object({
     title: Joi.string().min(5).max(255).required(),
-    status: Joi.string().valid("pending", "completed").required(),
+    status: Joi.string().valid("pending", "completed"),
   });
   return schema.validate(req);
 }
