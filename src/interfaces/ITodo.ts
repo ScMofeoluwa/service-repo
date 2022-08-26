9dda9de9f324396c2d5b002b3a224a0fe6ef9619
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-08-26 11:41:23 +0100

    chore: updated todo interface

--- src/interfaces/ITodo.ts
+++ src/interfaces/ITodo.ts
@@ -2,7 +2,7 @@ import { Todo } from "../entities/todo.entity";
 
 export interface ITodoService {
   createTodo(data: Omit<Todo, "_id">): Promise<Todo>;
-  getTodos(): Promise<Todo[]>;
+  getTodos(userId: string): Promise<Todo[]>;
   getTodo(id: string): Promise<Todo>;
   deleteTodo(id: string): Promise<boolean>;
   updateTodo(id: string, data: Partial<Todo>): Promise<boolean>;
