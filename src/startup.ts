2584b01f9492fcd1e82dff929e8090b4d66e53d2
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-09-16 13:16:19 +0100

    chore: proper linting

--- src/startup.ts
+++ src/startup.ts
@@ -3,13 +3,11 @@ import "./controller/user.controller";
 import "./controller/todo.controller";
 import "./controller/auth.controller";
 import { App } from "./application";
-import { configuration as conf} from "./config/config";
+import { configuration as conf } from "./config/config";
 
 export async function startup(port: number) {
   const app = new App();
   await app.setup(port);
 }
-
-
 
 startup(conf.app_port);
